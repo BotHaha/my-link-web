ff278cb4b95ccb44d3f2a079981674786e2b6bcd
BotHaha <95127133+BotHaha@users.noreply.github.com>
2025-03-30 11:50:05 +0800

    Add files via upload

---
+++ head-sticker.js
@@ -0,0 +1,8 @@
+function showOverlay() {
+    document.getElementById("imageOverlay").style.display = "flex";
+}
+
+function hideOverlay(e) {
+    const overlay = document.getElementById("imageOverlay");
+    overlay.style.display = "none";
+}
